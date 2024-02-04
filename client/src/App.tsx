b0631be4dcb11a4178958870bbb089d493e2aee0
박영기 <dudrl9415@naver.com>
2024-02-04 18:29:14 +0900

    Add Get Customers Rest API Module

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,4 +1,4 @@
-import React, { Fragment } from "react";
+import React, { Fragment, useState, useEffect } from "react";
 import "./App.css";
 import Customer from "./components/Customer";
 import Paper from "@material-ui/core/Paper";
@@ -37,40 +37,11 @@ interface CustomerArray {
   id: string;
   image: string;
 }
-const customers: CustomerArray[] = [
-  {
-    id: "dudrl1",
-    image: "https://placeimg.com/64/64/any",
-    name: "홍길동",
-    birthday: "941003",
-    gender: "남자1",
-    job: "대학생2",
-  },
-  {
-    id: "dudrl2",
-    image: "https://placeimg.com/64/64/1",
-    name: "나동빈",
-    birthday: "001003",
-    gender: "여자22",
-    job: "백수",
-  },
-  {
-    id: "dudrl3",
-    image: "https://placeimg.com/64/64/2",
-    name: "테스터",
-    birthday: "981003",
-    gender: "중성",
-    job: "사업자",
-  },
-  {
-    id: "dudrl444",
-    image: "https://placeimg.com/64/64/3",
-    name: "후니",
-    birthday: "241003",
-    gender: "강아지",
-    job: "음식점",
-  },
-];
+
+interface Customers {
+  customers: Array<CustomerArray>;
+}
+
 const customer: {
   name: string;
   birthday: string;
@@ -89,6 +60,32 @@ const customer: {
 
 function App(props: any) {
   const { classes } = props;
+  const stateInit: Customers = {
+    customers: [
+      {
+        id: "",
+        image: "",
+        name: "",
+        birthday: "",
+        gender: "",
+        job: "",
+      },
+    ],
+  };
+  const [state, setState] = useState(stateInit);
+
+  useEffect(() => {
+    callApi()
+      .then((res) => setState({ customers: res }))
+      .catch((err) => console.log(err));
+  }, []);
+
+  const callApi = async () => {
+    const response = await fetch("/api/customers");
+    const body = await response.json();
+    return body;
+  };
+
   return (
     <Paper className={classes.root}>
       <Table className={classes.table}>
@@ -103,19 +100,21 @@ function App(props: any) {
           </TableRow>
         </TableHead>
         <TableBody>
-          {customers.map((customer, idx) => {
-            return (
-              <Customer
-                key={customer.id + "_" + idx}
-                id={customer.id}
-                image={customer.image}
-                name={customer.name}
-                birthday={customer.birthday}
-                gender={customer.gender}
-                job={customer.job}
-              />
-            );
-          })}
+          {state.customers
+            ? state.customers.map((customer: CustomerArray, idx: number) => {
+                return (
+                  <Customer
+                    key={customer.id + "_" + idx}
+                    id={customer.id}
+                    image={customer.image}
+                    name={customer.name}
+                    birthday={customer.birthday}
+                    gender={customer.gender}
+                    job={customer.job}
+                  />
+                );
+              })
+            : null}
         </TableBody>
       </Table>
       {/* <Customer
--- server.js
+++ server.js
@@ -6,8 +6,41 @@ const port = process.env.PORT || 5000;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get("/api/hello", (req, res) => {
-  res.send({ message: "Hello Express!" });
+app.get("/api/customers", (req, res) => {
+  res.send([
+    {
+      id: "dudrl1",
+      image: "https://placeimg.com/64/64/any",
+      name: "홍길동",
+      birthday: "941003",
+      gender: "남자1",
+      job: "대학생2",
+    },
+    {
+      id: "dudrl2",
+      image: "https://placeimg.com/64/64/1",
+      name: "나동빈",
+      birthday: "001003",
+      gender: "여자22",
+      job: "백수",
+    },
+    {
+      id: "dudrl3",
+      image: "https://placeimg.com/64/64/2",
+      name: "테스터",
+      birthday: "981003",
+      gender: "중성",
+      job: "사업자",
+    },
+    {
+      id: "dudrl444",
+      image: "https://placeimg.com/64/64/3",
+      name: "후니",
+      birthday: "241003",
+      gender: "강아지",
+      job: "음식점",
+    },
+  ]);
 });
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
